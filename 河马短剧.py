01ecf6b991c44a48f6f5a20aaf33e81848a20923
cefiro131421 <62463745+cefiro131421@users.noreply.github.com>
2025-11-15 23:20:33 +0800

    Add files via upload

---
+++ 河马短剧.py
@@ -0,0 +1,414 @@
+<!DOCTYPE html>
+<html lang='zh-CN'>
+<head>
+<meta content='#303643' name='theme-color'>
+<link href='/static/images/logo_icon.png' rel='icon' sizes='192x192'>
+<link href='/static/images/logo_icon.png' rel='apple-touch-icon' sizes='192x192'>
+<title>yiwu369/6758 - Gitee.com</title>
+<meta charset='utf-8'>
+<meta content='always' name='referrer'>
+<meta content='Gitee' property='og:site_name'>
+<meta content='Object' property='og:type'>
+<meta content='https://gitee.com/yiwu369/6758/blob/master/%E9%9D%92%E9%BE%99/%E6%B2%B3%E9%A9%AC%E7%9F%AD%E5%89%A7.py' property='og:url'>
+<meta content='https://foruda.gitee.com/avatar/1759992885468742163/13823886_yiwu369_1759992885.jpeg' itemprop='image' property='og:image'>
+<meta content='yiwu369/6758' itemprop='name' property='og:title'>
+<meta content='Gitee.com（码云） 是 OSCHINA.NET 推出的代码托管平台，支持 Git 和 SVN，提供免费的私有仓库托管。目前已有超过 1350万的开发者选择 Gitee。' property='og:description'>
+<meta content='6758' name='Keywords'>
+<meta content='Gitee.com（码云） 是 OSCHINA.NET 推出的代码托管平台，支持 Git 和 SVN，提供免费的私有仓库托管。目前已有超过 1350万的开发者选择 Gitee。' itemprop='description' name='Description'>
+<meta content='pc,mobile' name='applicable-device'>
+
+
+<meta name="csrf-param" content="authenticity_token" />
+<meta name="csrf-token" content="OL9uztoJcW0qLRK0Gnrwd0a6/e6m9O5DplTd6FkU4bzxBl50m0qz14Md4gFTm5ykSFige0RbjbNUFPx7Ogfltw==" />
+
+<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="https://cn-assets.gitee.com/assets/favicon-9007bd527d8a7851c8330e783151df58.ico" />
+<script>
+//<![CDATA[
+window.gon = {};gon.locale="zh-CN";gon.sentry_dsn=null;gon.baidu_register_hm_push=null;gon.info={"controller_path":"mobile/blob","action_name":"show","current_user":{"id":16251579,"name":"张子剑","username":"F1-F","official":false,"admin":false,"avatar_url":"https://foruda.gitee.com/avatar/1758293266087631571/16251579_f1-f_1758293265.gif"}};gon.currentUser={"id":16251579,"name":"张子剑","nickname":"张子剑","username":"F1-F","avatar_url":"https://foruda.gitee.com/avatar/1758293266087631571/16251579_f1-f_1758293265.gif!avatar100","widget_url":"","large_widget_url":"","path":"/F1-F","is_admin":false,"limited":true,"color_scheme_class":"white","block":false};gon.nothingTxt="啥也没有";gon.http_clone="https://gitee.com/yiwu369/6758.git";gon.user_project="yiwu369/6758";gon.manage_branch="管理分支";gon.manage_tag="管理标签";gon.enterprise_id=0;gon.create_reaction_path="/yiwu369/6758/reactions";gon.ipipe_base_url="https://go-api.gitee.com";gon.artifact_base_url="https://go-repo.gitee.com";gon.gitee_go_remote_url="https://go.gitee.com/assets";gon.gitee_go_active=false;gon.current_project_is_mirror=false;gon.show_repo_comment=false;gon.diagram_viewer_path="https://diagram-viewer.giteeusercontent.com";gon.ent_host="e.gitee.com";
+//]]>
+</script>
+<script src="//res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
+<script>
+  var title = document.title.replace(/( - Gitee| - 码云)$/, '')
+      imgUrl = '';
+  
+  document.addEventListener('DOMContentLoaded', function(event) {
+    var imgUrlEl = document.querySelector('.readme-box .markdown-body > img, .readme-box .markdown-body :not(a) > img');
+    imgUrl = imgUrlEl && imgUrlEl.getAttribute('src');
+  
+    if (!imgUrl) {
+      imgUrlEl = document.querySelector('meta[itemprop=image]');
+      imgUrl = imgUrlEl && imgUrlEl.getAttribute('content');
+      imgUrl = imgUrl || "https://gitee.com/static/images/logo_themecolor_circle.png";
+    }
+  
+    wx.config({
+      debug: false,
+      appId: "wxff219d611a159737",
+      timestamp: "1763205257",
+      nonceStr: "b42180a89f88704fcf755519b1af2b2d",
+      signature: "5b3ba30790b761d9370838c361a147f460e662dc",
+      jsApiList: [
+        'onMenuShareTimeline',
+        'onMenuShareAppMessage'
+      ]
+    });
+  
+    wx.ready(function () {
+      wx.onMenuShareTimeline({
+        title: title, // 分享标题
+        link: "https://gitee.com/yiwu369/6758/blob/master/%E9%9D%92%E9%BE%99/%E6%B2%B3%E9%A9%AC%E7%9F%AD%E5%89%A7.py", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        imgUrl: imgUrl // 分享图标
+      });
+      wx.onMenuShareAppMessage({
+        title: title, // 分享标题
+        link: "https://gitee.com/yiwu369/6758/blob/master/%E9%9D%92%E9%BE%99/%E6%B2%B3%E9%A9%AC%E7%9F%AD%E5%89%A7.py", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+        desc: document.querySelector('meta[name=Description]').getAttribute('content'),
+        imgUrl: imgUrl // 分享图标
+      });
+    });
+    wx.error(function(res){
+      console.error('err', res)
+    });
+  })
+</script>
+
+
+<script>
+  !function(a,b){function c(){var b=f.getBoundingClientRect().width;b/i>540&&(b=540*i);var c=b/10;f.style.fontSize=c+"px",k.rem=a.rem=c}var d,e=a.document,f=e.documentElement,g=e.querySelector('meta[name="viewport"]'),h=e.querySelector('meta[name="flexible"]'),i=0,j=0,k=b.flexible||(b.flexible={});if(g){console.warn("将根据已有的meta标签来设置缩放比例");var l=g.getAttribute("content").match(/initial\-scale=([\d\.]+)/);l&&(j=parseFloat(l[1]),i=parseInt(1/j))}else if(h){var m=h.getAttribute("content");if(m){var n=m.match(/initial\-dpr=([\d\.]+)/),o=m.match(/maximum\-dpr=([\d\.]+)/);n&&(i=parseFloat(n[1]),j=parseFloat((1/i).toFixed(2))),o&&(i=parseFloat(o[1]),j=parseFloat((1/i).toFixed(2)))}}if(!i&&!j){var p=a.navigator.userAgent,q=(!!p.match(/android/gi),!!p.match(/iphone/gi)),r=q&&!!p.match(/OS 9_3/),s=a.devicePixelRatio;i=q&&!r?s>=3&&(!i||i>=3)?3:s>=2&&(!i||i>=2)?2:1:1,j=1/i}if(f.setAttribute("data-dpr",i),!g)if(g=e.createElement("meta"),g.setAttribute("name","viewport"),g.setAttribute("content","initial-scale="+j+", maximum-scale="+j+", minimum-scale="+j+", user-scalable=no"),f.firstElementChild)f.firstElementChild.appendChild(g);else{var t=e.createElement("div");t.appendChild(g),e.write(t.innerHTML)}a.addEventListener("resize",function(){clearTimeout(d),d=setTimeout(c,300)},!1),a.addEventListener("pageshow",function(a){a.persisted&&(clearTimeout(d),d=setTimeout(c,300))},!1),"complete"===e.readyState?e.body.style.fontSize=12*i+"px":e.addEventListener("DOMContentLoaded",function(){e.body.style.fontSize=12*i+"px"},!1),c(),k.dpr=a.dpr=i,k.refreshRem=c,k.rem2px=function(a){var b=parseFloat(a)*this.rem;return"string"==typeof a&&a.match(/rem$/)&&(b+="px"),b},k.px2rem=function(a){var b=parseFloat(a)/this.rem;return"string"==typeof a&&a.match(/px$/)&&(b+="rem"),b}}(window,window.lib||(window.lib={}));
+</script>
+
+<script src="https://cn-assets.gitee.com/assets/mobile/application-89d6391495c1c3797ff577cbae8221cf.js"></script>
+<link rel="stylesheet" media="all" href="https://cn-assets.gitee.com/assets/mobile/main-3bc98462ad991debd85d7ce7af7a44a4.css" />
+
+<script type='text/x-mathjax-config'>
+MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    displayMath: [["$$","$$"],["\\[","\\]"]],
+    processEscapes: true,
+    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code'],
+    ignoreClass: "container|files",
+    processClass: "markdown-body"
+  }
+});
+</script>
+<script src="https://cn-assets.gitee.com/uploads/resources/MathJax-2.7.2/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+
+<link rel="stylesheet" media="all" href="https://cn-assets.gitee.com/webpacks/css/gitee_nps-ae0dbee40f6ddc72015a.css" />
+<script src="https://cn-assets.gitee.com/webpacks/gitee_nps-03ff804099144a49b055.bundle.js"></script>
+<script src="https://cn-assets.gitee.com/webpacks/gitee_icons-522e3725c14e62fdcf2b.bundle.js"></script>
+</head>
+<body class='mobile'>
+<header class='gitee-nav'>
+<div class='gitee-nav__background'></div>
+<div class='left-side item side-slide-btn'>
+<i class='iconfont icon-mode-table'></i>
+</div>
+<div class='center item nav-title'>
+<a class="default nav-title-link" href="/"><img alt="Gitee - 基于 Git 的代码托管和研发协作平台" title="Gitee - 基于 Git 的代码托管和研发协作平台" class="light-mode-img" src="/static/images/logo-black.svg?t=158106664" />
+<img alt="Gitee - 基于 Git 的代码托管和研发协作平台" title="Gitee - 基于 Git 的代码托管和研发协作平台" class="dark-mode-img" src="/static/images/logo.svg?t=158106664" />
+</a></div>
+<div class='right-side item'>
+<a class="item notifications" id="noti" href="/notifications?from=home"><i class='iconfont icon-remind'></i>
+<div class='red noti-label' id='unread-noti'></div>
+</a></div>
+<div class='slider-box'>
+<div class='content'>
+<div class='top-area'>
+<div class='avatar-box'>
+<a href="/F1-F"><img class="ui avatar image F1-F-avatar" src="https://foruda.gitee.com/avatar/1758293266087631571/16251579_f1-f_1758293265.gif" alt="16251579 f1 f 1758293265" />
+</a></div>
+<div class='info-box'>
+<a href="/F1-F">张子剑</a>
+</div>
+</div>
+<div class='middle-area'>
+<div class='action-list'>
+<ul>
+<li>
+<a href="/F1-F/dashboard"><i class='iconfont icon-ic-dashboard'></i>
+<span class='item-name'>我的工作台</span>
+</a></li>
+<li>
+<a href="/explore"><i class='iconfont icon-ic-discover'></i>
+<span class='item-name'>开源</span>
+</a></li>
+<li>
+<a href="/enterprises"><i class='iconfont icon-ic-enterprise'></i>
+<span class='item-name'>企业版</span>
+</a></li>
+<li>
+<a href="/education"><i class='iconfont icon-ic-education'></i>
+<span class='item-name'>高校版</span>
+</a></li>
+<li class='split-line'></li>
+<li>
+<a href="/search"><i class='iconfont icon-ic-search'></i>
+<span class='item-name'>搜索</span>
+</a></li>
+<li>
+<a href="/terms"><i class='iconfont icon-file'></i>
+<span class='item-name'>使用条款</span>
+</a></li>
+<li>
+<a href="/about_us"><i class='iconfont icon-issuepx'></i>
+<span class='item-name'>关于我们</span>
+</a></li>
+<li>
+<a href="/profile"><i class='iconfont icon-edit'></i>
+<span class='item-name'>设置</span>
+</a></li>
+<li class='theme-selector-mobile'>
+<a class="theme-selector-btn-mobile" onclick="toggleThemeSelectorMobile()" title="主题设置" href="javascript:void(0)"><i class='fa fa-moon-o' id='theme-icon-mobile'></i>
+<span class='item-name'>主题</span>
+<i class='fa fa-angle-down theme-arrow-mobile' id='theme-arrow-mobile'></i>
+</a><div class='theme-options-mobile' id='theme-options-mobile' style='display: none;'>
+<a class="theme-option-mobile" onclick="setTheme(&quot;system&quot;)" data-theme="system" id="theme-system-option-mobile" href="javascript:void(0)"><i class='fa fa-adjust' id='theme-system-icon-mobile'></i>
+<span class='item-name'>跟随系统</span>
+</a><a class="theme-option-mobile" onclick="setTheme(&quot;light&quot;)" data-theme="light" id="theme-light-option-mobile" href="javascript:void(0)"><i class='fa fa-sun-o' id='theme-light-icon-mobile'></i>
+<span class='item-name'>亮色模式</span>
+</a><a class="theme-option-mobile" onclick="setTheme(&quot;dark&quot;)" data-theme="dark" id="theme-dark-option-mobile" href="javascript:void(0)"><i class='fa fa-moon-o' id='theme-dark-icon-mobile'></i>
+<span class='item-name'>暗黑模式</span>
+</a></div>
+<li>
+<a class="destroy-user-session" rel="nofollow" data-method="delete" href="/logout"><i class='iconfont icon-ic-logout'></i>
+<span class='item-name'>退出</span>
+</a></li>
+</li>
+</ul>
+</div>
+</div>
+<div class='bottom-area'>
+<div class='slider-ctrl slider-close-btn'>
+<i class='fa fa-angle-double-left'></i>
+</div>
+</div>
+</div>
+</div>
+</header>
+<script>
+  var isLogin = true,
+      $slideBtn = $('body.mobile .gitee-nav .side-slide-btn'),
+      $sliderBox = $('body.mobile .gitee-nav .slider-box'),
+      $sliderContent = $('body.mobile .gitee-nav .slider-box > .content'),
+      $sliderClaseBtn = $('body.mobile .gitee-nav .slider-box > .content .bottom-area'),
+      $notiBox = $('#unread-noti'),
+      $navTitle = $('body > .gitee-nav > .nav-title'),
+      notifierObj = {
+        num: 0
+      };
+  function openSlider() {
+    $slideBtn.addClass('on');
+    $sliderBox.addClass('on');
+  }
+  function closeSlider() {
+    $slideBtn.removeClass('on');
+    $sliderBox.removeClass('on');
+  }
+  if ($slideBtn.length > 0) {
+    $slideBtn.on('click', function(e) {
+      var $target = $(e.target),
+        status = $target.hasClass('on');
+      status = !status;
+      if (status) {
+        openSlider()
+      } else {
+        closeSlider()
+      }
+    })
+    $sliderBox.on('click', function(e) {
+      if (e.target === $sliderBox[0]) {
+        closeSlider();
+      }
+    })
+    $sliderClaseBtn.on('click', function() {
+      closeSlider();
+    })
+  }
+  isLogin && $.getJSON ('/notifications/unread_count', function(data){
+    if (data.total > 0) {
+      var title = (data.infos + data.referer) + ' 未读通知 ' +
+              data.messages + ' 条私信'
+      $('#noti').attr('title', title)
+      notifierObj.num = data.total;
+    }
+  })
+  Object.defineProperty(notifierObj, 'num', {
+    get: function() {
+      return notifierObj['_num'] === undefined ? 0 : notifierObj['_num'];
+    },
+    set: function(val) {
+      var num = Math.floor(globalLib.str2num(val));
+      if (num >= 0) {
+        notifierObj['_num'] = num;
+      }
+      if (num === 0) {
+        $notiBox.hide();
+      } else if (num > 0) {
+        if (num < 100) $notiBox.text(num).attr('style', '').show();
+        else $notiBox.text('..').attr('style', 'line-height: 0.9 !important;').show();
+      }
+    }
+  })
+  window.notifier = {
+    add: function (val) {
+      if (val === undefined) val = 1;
+      if (globalLib.isNum(val)) {
+        val = Math.floor(globalLib.str2num(val));
+        notifierObj.num += val;
+        return notifierObj.num;
+      } else return;
+    },
+    reduce: function(val) {
+      if (val === undefined) val = 1;
+      if (globalLib.isNum(val)) {
+        val = Math.floor(globalLib.str2num(val));
+        notifierObj.num = notifierObj.num - val > 0 ? notifierObj.num - val : 0;
+        return notifierObj.num;
+      } else return;
+    },
+    set: function(val) {
+      if (val === undefined) return;
+      if (globalLib.isNum(val)) {
+        notifierObj.num = val;
+      }
+    }
+  },
+  window.navTitle = {
+    default: {
+      link: '/',
+      image: '/static/images/logo.svg',
+      class: 'default'
+    },
+    set: function(options) {
+      function set2nav(options) {
+        var $a = $('<a/>', {
+          class: 'nav-title-link',
+          href: options.link || 'javascript:void(0)'
+        });
+        if (options.template) {
+          //如果存在字符模板
+          try {
+            $a.html(options.template);
+          } catch(e) {
+            return;
+          }
+        } else {
+          //如果没有字符模板
+          if (options.class) {
+            $a.addClass(options.class);
+          }
+          if (options.image) {
+            $a.append($('<img/>', {
+              class: 'nav-title-image',
+              src: options.image,
+            }))
+          } else if (options.icon) {
+            $a.append($('<i/>', {
+              class: options.icon + ' nav-title-icon'
+            }))
+          }
+          if (options.text) {
+            $a.append($('<span/>', {
+              class: 'nav-title-text',
+              text: options.text
+            }))
+          }
+        }
+        $navTitle.html($a);
+      }
+  
+      if (typeof options === 'string' && options === 'default') {
+        set2nav(this.default)
+      } else if (typeof options === 'object') {
+        try {
+          set2nav(options)
+        } catch(err) {
+          console.error(err);
+        }
+      }
+    }
+  }
+  $('.destroy-user-session').on('click', function() {
+    localStorage.setItem('theme', 'system');
+    $.cookie('access_token', null, { path: '/' });
+  })
+</script>
+
+<div class='main-container'>
+<link rel="stylesheet" media="all" href="https://cn-assets.gitee.com/webpacks/css/mobile_projects.ce30da2.css" />
+<div class='container'>
+<div class='projects-page' id='content'>
+<div id='projects-page'></div>
+</div>
+</div>
+<script>
+  window.pageParams = {
+    mobile: true,
+    project: {
+      id: '38010018',
+      name: '6758',
+      description: "",
+      icon: 'project-icon iconfont icon-project-public',
+      gvp: false,
+      recommend: false,
+      inEnterprise: false,
+      inEducation: false,
+      can_admin_issue: false,
+      name_with_namespace: "yiwu369/6758",
+      default_branch: 'master',
+      enableComment: false,
+      path: '/yiwu369/6758',
+      openIssueCount: 1,
+      activePullCount: 0,
+      is_block: false,
+      block_name: '取消屏蔽',
+      emptyRepo: false,
+      issue_comment: true,
+      is_started: true,
+      is_readonly_pull_request: false,
+      status_name: "开始",
+      is_readonly_issue: false,
+    }
+  }
+</script>
+<script src="https://cn-assets.gitee.com/webpacks/vendors_lib-7ff466a6da368d391eda.js"></script>
+<script src="https://cn-assets.gitee.com/webpacks/mobile_projects.37f7b11.bundle.js"></script>
+
+</div>
+<footer class='gitee-footer clearfix'>
+<div class='left-side side'>
+<a href="/"><img src="/static/images/logo-en.svg" alt="Logo en" />
+<span class='copyright'>
+&copy; Gitee.com
+</span>
+</a></div>
+<div class='right-side side'>
+<a href="/yiwu369/6758/blob/master/%E9%9D%92%E9%BE%99/%E6%B2%B3%E9%A9%AC%E7%9F%AD%E5%89%A7.py?skip_mobile=true">桌面版</a>
+</div>
+</footer>
+<script>
+  flash_time=2000
+</script>
+
+<script>
+  !function() {
+    var flash_message = ""
+    if (flash_message == 'captcha_fail')
+      flash_message = "验证码不正确"
+    else if (flash_message == 'captcha_expired')
+      flash_message = "验证码已过期，请点击刷新"
+    else if (flash_message == 'not_found_in_database')
+      flash_message = "帐号或者密码错误"
+    else if (flash_message == 'not_found_and_show_captcha')
+      flash_message = "帐号或者密码错误"
+    else if (flash_message == 'phone_captcha_fail')
+      flash_message = "手机验证码不通过"
+    if (!$.isEmptyObject(flash_message)) gon.flash_message = flash_message
+  }()
+</script>
+
+</body>
+</html>
+
